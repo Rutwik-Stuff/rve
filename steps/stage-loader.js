2bc12c9a1b7957b6a452b63b9a588f83f1cdd6c7
7Zeb <66693249+7zeb@users.noreply.github.com>
2025-10-17 08:48:00 -0500

    fix import paths

--- steps/stage-loader.js
+++ steps/stage-loader.js
@@ -35,22 +35,22 @@ function getCurrentStage() {
 function loadStage(stage) {
   switch (stage) {
     case "Import":
-      import("./steps/import.js");
+      import("/rve/steps/import.js");
       break;
     case "Edit":
-      import("./steps/edit.js");
+      import("/rve/steps/edit.js");
       break;
     case "Music":
-      import("./steps/music.js");
+      import("/rve/steps/music.js");
       break;
     case "Final Checks":
-      import("./steps/final.js");
+      import("/rve/steps/final.js");
       break;
     case "Export":
-      import("./steps/export.js");
+      import("/rve/steps/export.js");
       break;
     case "Upload":
-      import("./steps/upload.js");
+      import("/rve/steps/upload.js");
       break;
     default:
       console.warn(`Unknown stage: ${stage}`);
